4782fab4a40c420ad226501896d5db318915c632
Sean <seanhung2000@gmail.com>
2021-09-14 16:11:43 +0800

    9/14

--- bank.sol
+++ bank.sol
@@ -3,6 +3,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Bank{
+    
+    event event_add_balance(address customer_address, uint256 amount);
+    event event_add_loan(address customer_address, uint256 amount);
+    event event_withdraw(address customer_address, uint256 amount);
+    event event_receive(address donor, uint256 amount);
+    
+    address payable owner;
     string public bank_name;
     uint8 day_interest_rate;
     uint8 day_lending_rate;
@@ -15,6 +22,11 @@ contract Bank{
         uint256 customer_lending_amount;
     }
     mapping(address => Customers) address_to_customers;
+    
+    modifier is_onwer(){
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
     
     constructor(string memory name){
         bank_name = name;
@@ -29,18 +41,21 @@ contract Bank{
         address_to_customers[customer_address].customer_lending_amount = 0;
     }   
     
-    function AddBalance(address payable customer_address, uint256 value) public {
+    function add_balance(address payable customer_address, uint256 value) public is_onwer{
         address_to_customers[customer_address].customer_balance += value;
+        emit event_add_balance(customer_address, value);
+        
     }
     
-    function AddLoan(address payable customer_address , uint256 value) public {
+    function add_loan(address payable customer_address , uint256 value) public is_onwer{
         address_to_customers[customer_address].customer_lending_amount += value;
+        emit event_add_loan(customer_address, value);
     }
-    function CheckBalance (address payable customer_address) public view returns(uint256) {
+    function check_balance (address payable customer_address) public view returns(uint256) {
         return address_to_customers[customer_address].customer_balance - address_to_customers[customer_address].customer_lending_amount;
     }
     
-    function CheckLoan (address payable customer_address) public view returns(uint256){
+    function check_loan (address payable customer_address) public view returns(uint256){
         return address_to_customers[customer_address].customer_lending_amount;
     }
     
@@ -59,9 +74,22 @@ contract Bank{
         return sum;
     }
     
+    receive() external payable{
+        emit event_receive(msg.sender, msg.value);
+    }
+    
+    fallback() external payable{
+        
+    }
+    
+    function destroy() external is_onwer{
+        selfdestruct(owner);
+    }
+    
     function withdraw() external returns(uint256){
         address user = payable(msg.sender);
         uint256 balance = address_to_customers[user].customer_balance - address_to_customers[user].customer_lending_amount;
+        require(balance > 0, "balance not enough");
         address_to_customers[user].customer_balance -= balance;
         return balance;
     }
--- company.sol
+++ company.sol
@@ -3,7 +3,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Company{
-    string public boss_name = 'aa';
+    
+    event event_performance(address sales_address, uint256 profit);
+    event event_add_sales(string name, address sales_address);
+    event event_withdraw(address sales_address, uint256 amount);
+    event event_receive(address donor, uint256 amount);
+    
+    string public boss_name;
     address payable public boss_address;
     address[] all_address;
     struct Sales{
@@ -16,17 +22,23 @@ contract Company{
     mapping(address => string) address_to_name;
     mapping(string => Sales) name_to_sales;
     
+    modifier is_boss(){
+        require(msg.sender == boss_address, "You are not the owner");
+        _;
+    }
+    
     constructor(string memory name){
         boss_name = name;
         boss_address = payable(msg.sender);
     }
     
-    function add_performance(string memory salesman , uint256 profit) public {
+    function add_performance(string memory salesman , uint256 profit) public is_boss{
         name_to_sales[salesman].performance += profit;
         name_to_sales[salesman].salary += profit/10;
+        emit event_performance(name_to_sales[salesman].sales_address, profit);
     }
     
-    function add_sales(string memory salesman , address payable sales_address ) public {
+    function add_sales(string memory salesman , address payable sales_address ) public is_boss{
         all_address.push(sales_address);
         address_to_name[sales_address] = salesman;
         name_to_sales[salesman].name = salesman;
@@ -34,6 +46,8 @@ contract Company{
         name_to_sales[salesman].performance = 0;
         name_to_sales[salesman].salary = 0;
         name_to_sales[salesman].withdrawn = 0;
+        
+        emit event_add_sales(salesman, sales_address);
     }
     
     function check_balance(address payable sales_address) public view returns(uint256){
@@ -58,12 +72,15 @@ contract Company{
     function withdraw() external returns(uint256){
         address payable user = payable(msg.sender);
         uint balance = check_balance(user);
+        require(balance > 0, "balance not enough");
         user.transfer(balance);
         name_to_sales[address_to_name[user]].withdrawn += balance;
+        emit event_withdraw(msg.sender, balance);
         return balance;
     }
     
     receive() external payable{
+        emit event_receive(msg.sender, msg.value);
     
     }
     
@@ -71,7 +88,7 @@ contract Company{
         
     }
     
-    function destroy() external{
+    function destroy() external is_boss{
         selfdestruct(boss_address);
     }
 }
